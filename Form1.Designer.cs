cc4b9e5967c057d018a7417aaf8f1d4e70bae07e
omar <omr_abdo_@yahoo.com>
2023-01-25 18:34:53 +0200

    add color

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -89,6 +89,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
             this.label1.Location = new System.Drawing.Point(419, 194);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 25);
