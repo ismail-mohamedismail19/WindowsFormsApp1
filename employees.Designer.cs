94a22c20dd880f7d871277e955ebf50deed55f1e
omar <omr_abdo_@yahoo.com>
2023-01-25 19:00:22 +0200

    changed text and fontstyle

--- employees.Designer.cs
+++ employees.Designer.cs
@@ -47,11 +47,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Teal;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(412, 73);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(345, 32);
+            this.label1.Size = new System.Drawing.Size(369, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "employee M S Version 1.0";
             this.label1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
@@ -60,13 +60,13 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Teal;
-            this.label2.Location = new System.Drawing.Point(412, 156);
+            this.label2.Location = new System.Drawing.Point(452, 153);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(345, 32);
+            this.label2.Size = new System.Drawing.Size(281, 32);
             this.label2.TabIndex = 1;
-            this.label2.Text = "employee M S Version 1.0";
+            this.label2.Text = "Manage Employees";
             this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
             // 
             // employees
