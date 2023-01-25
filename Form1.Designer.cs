d92350c57aab006ed75e1d961da94152ee532607
omar <omr_abdo_@yahoo.com>
2023-01-25 18:32:14 +0200

    add font for textbox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -68,9 +68,10 @@
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(422, 223);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(240, 23);
+            this.textBox1.Size = new System.Drawing.Size(240, 30);
             this.textBox1.TabIndex = 3;
             // 
             // Form1
