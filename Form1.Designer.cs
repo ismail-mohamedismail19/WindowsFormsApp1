f820e02c2a12867d93b03d6647433f21c91d15f7
omar <omr_abdo_@yahoo.com>
2023-01-25 18:19:03 +0200

    add font size for label

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,10 +44,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Teal;
             this.label2.Location = new System.Drawing.Point(391, 146);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(64, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
