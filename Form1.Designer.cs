78d45df11fe7942e978f7289f1a933e7ad2baf25
omar <omr_abdo_@yahoo.com>
2023-01-26 20:35:56 +0200

    change font size

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -154,10 +154,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(114, 169);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 17);
+            this.label4.Size = new System.Drawing.Size(130, 25);
             this.label4.TabIndex = 10;
             this.label4.Text = "Emplotee MS";
             // 
